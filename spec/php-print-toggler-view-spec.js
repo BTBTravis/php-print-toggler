ab06f9ac5867ea809d358d50fed2c6bbb67dec36
btbtravis <btbtravis@gmail.com>
2017-08-23 13:58:06 -0500

    Added the hide php prints command

---
+++ spec/php-print-toggler-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import PhpPrintTogglerView from '../lib/php-print-toggler-view';
+
+describe('PhpPrintTogglerView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
